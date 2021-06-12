69384732fc28b852460879f95b6e61cf640fc89f
Zantaclaus <tarzuto@gmail.com>
2021-06-13 00:42:49 +0700

    Butite Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,35 @@
-import React, { useEffect } from "react";
+import React from "react";
 import loginImage from "../images/loginImage.svg";
 import "../css/login.css";
 
 function Login(props) {
-  return <h1>START</h1>;
+  return (
+    <div className="login-page">
+      <div className="img-container">
+        <img src={loginImage} alt="" />
+      </div>
+      <form className="login-form">
+        <h1>Welcome to Grader!</h1>
+        <h3>Register your account</h3>
+        <div className="login-input username-input">
+          <label htmlFor="" className="">
+            Username
+          </label>
+          <input type="text" placeholder="username" />
+        </div>
+        <div className="login-input password-input">
+          <label htmlFor="" className="">
+            Password
+          </label>
+          <input type="password" placeholder="password" />
+        </div>
+        <a href="/" className="forgot">
+          Forgot Password?
+        </a>
+        <input type="submit" className="login-button" value="Login" />
+      </form>
+    </div>
+  );
 }
 
 export default Login;
