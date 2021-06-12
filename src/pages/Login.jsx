56adc9bee434aff9e4f864c1c84c4fd69fba8e88
Zantaclaus <tarzuto@gmail.com>
2021-06-12 22:51:06 +0700

    Setup Project

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import loginImage from "../images/loginImage.svg";
+import "../css/login.css";
+
+function Login(props) {
+  return <h1>START</h1>;
+}
+
+export default Login;
