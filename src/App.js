c84c7b1094b84d22e99807e2483df3633230b955
Zantaclaus <tarzuto@gmail.com>
2021-06-13 18:53:41 +0700

    Install JwtDecode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { Switch, Route, Redirect } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/" component={Home} />
-        <Redirect path="/" exact to="/home" />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (ex) {}
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Home} />
+          <Redirect path="/" exact to="/home" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
