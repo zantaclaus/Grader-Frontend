b61bfa2cd7bf29eb718c0130fca4f11413bbb75c
Zantaclaus <tarzuto@gmail.com>
2021-06-13 23:13:08 +0700

    Built Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../css/navbar.css";
 
 function Navbar(props) {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h3>CE BOOTSUP</h3>
+      </div>
+      <div className="nav-list">
+        <ul>
+          <li className="nav-item">
+            <Link to="/home">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/home">Tasks</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/home">GruideBook</Link>{" "}
+          </li>
+          <li className="nav-item">
+            <Link to="/home">Profile</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login">Logout</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
