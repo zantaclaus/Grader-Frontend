8692e6cb168f4e559cc23815d13c9d3d70c20f63
Zantaclaus <tarzuto@gmail.com>
2021-06-14 18:41:14 +0700

    Create Hero Component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Aos from "aos";
+import hero from "../images/hero.svg";
+import "../css/home.css";
+
+function Hero(props) {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+  return (
+    <div>
+      <div className="block hero">
+        <div className="container grid grid--1x2">
+          <header className="hero__content">
+            <h1 data-aos="fade-right">
+              Programming <span>Tasks</span>
+            </h1>
+            <p>Be a good programmer in 2 weeks.</p>
+            <Link to="/login" className="btn btn--primary btn--stretched">
+              Get Started
+            </Link>
+          </header>
+          <img data-aos="fade-left" className="hero__image" src={hero} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 function Home(props) {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+  return <Hero />;
 }
 
 export default Home;
