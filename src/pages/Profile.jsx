b11a7b0dbf8611be39b231b2b0b955c7f392a37d
Zantaclaus <tarzuto@gmail.com>
2021-06-15 14:20:06 +0700

    comment Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Profile(props) {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+  return <div>{/* <h1>Profile</h1> */}</div>;
 }
 
 export default Profile;
