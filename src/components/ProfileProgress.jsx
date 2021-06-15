627e98741590fcb825156e54ee5e2ddb12a04008
Zantaclaus <tarzuto@gmail.com>
2021-06-15 18:28:27 +0700

    Create ProfileProgress

---
+++ src/components/ProfileProgress.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ProfileProgress(props) {
+  return (
+    <div>
+      <div className="card card__profile">
+        <div className="card__title">
+          <h1>Your Progress</h1>
+        </div>
+        <div className="card__content">
+          <div className="box"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileProgress;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ProfileBox from "../components/ProfileBox";
 import ProfileRank from "../components/ProfileRank";
+// import ProfileProgress from "../components/ProfileProgress";
 
 function Profile(props) {
   return (
     <div className="profile__container">
       <ProfileBox />
       <ProfileRank />
+      <ProfileRank />
+      {/* <ProfileProgress /> */}
     </div>
   );
 }
