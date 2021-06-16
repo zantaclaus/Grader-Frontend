060dc4ebc9fc146f8ff96c18e2767ab7d8d5f93f
Zantaclaus <tarzuto@gmail.com>
2021-06-16 22:05:14 +0700

    Built bold varible

--- src/components/ProfileRank.jsx
+++ src/components/ProfileRank.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Aos from "aos";
-// import rank from "../images/rank.png";
+import team1 from "../images/team1.png";
 import "../css/profileRank.css";
 
 function ProfileRank(props) {
@@ -14,12 +14,9 @@ function ProfileRank(props) {
           <h1>Your Rank</h1>
         </div>
         <div className="card__content rank__content">
-          <div className="rank__order">
+          <div className="rank__team">
             <h3>You are</h3>
-            <h1>
-              1<sup>st</sup>
-            </h1>
-            {/* <img src={rank} alt="" /> */}
+            <img className="team__logo" src={team1} alt="" />
           </div>
           <div className="rank__score">
             <h3>Your score</h3>
