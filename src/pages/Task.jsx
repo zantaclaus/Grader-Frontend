989e1a47f5ab0572bf1ebaa226656df1a235ae8b
Zantaclaus <tarzuto@gmail.com>
2021-06-18 21:28:03 +0700

    Chage api of getTaskById

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -7,18 +7,20 @@ import "../css/task.css";
 
 function Task(props) {
   const task = useSelector((state) => state.task);
+  const user = useSelector((state) => state.user.user);
   const { id } = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchTask = async () => {
       const response = await axios
-        .get(`http://localhost:5000/api/question-id/userId/${id}`)
+        .get(`http://localhost:5000/api/question-id/${user.id}/${id}`)
         .catch((error) => console.log("Error", error));
 
       dispatch(selectedTasks(response.data[0]));
     };
     fetchTask();
+    console.log("user id", user);
   }, [dispatch, id]);
 
   return (
