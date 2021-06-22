d1d8d0c9c679c49cd0ea6a58dddc1dbaf3c75497
Zantaclaus <tarzuto@gmail.com>
2021-06-23 01:18:41 +0700

    Change editor theme.

--- src/components/TaskEditor.jsx
+++ src/components/TaskEditor.jsx
@@ -3,7 +3,7 @@ import "../css/taskEditor.css";
 
 import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/themes/prism-dark.css";
+import "prismjs/themes/prism.css";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-c";
 
