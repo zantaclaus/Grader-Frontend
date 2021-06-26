d83d7083012c31df428bb22c16d1d401500d4fb0
Zantaclaus <tarzuto@gmail.com>
2021-06-26 14:23:44 +0700

    Imprement Pageination Component.

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import "../css/pagination.css";
 import { IoMdArrowDropleftCircle } from "react-icons/io";
 import { IoMdArrowDroprightCircle } from "react-icons/io";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { pageIncrement, pageDecrement } from "../redux/actions/pageAction";
 
 function Pagination(props) {
-  const page = useSelector((state) => state.page);
   const dispatch = useDispatch();
 
   return (
