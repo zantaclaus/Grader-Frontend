537662c842fac941c75bb3caf9848da7fc330fd2
Zantaclaus <tarzuto@gmail.com>
2021-06-26 15:53:15 +0700

    Show Unit selected.

--- src/components/FilterBox.jsx
+++ src/components/FilterBox.jsx
@@ -1,46 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/filterBox.css";
 import { FaCaretDown } from "react-icons/fa";
+import { getUnits } from "../services/unitService";
 import { addToggle, delClass } from "../services/toggleService";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setTitle, SetTitle } from "../redux/actions/dropdownAction";
 
 function FilterBox(props) {
+  const [units, setUnits] = useState([]);
+  const dispatch = useDispatch();
+  const title = useSelector((state) => state.title);
+
+  useEffect(() => {
+    const units = [{ name: "All Units" }, ...getUnits()];
+    setUnits(units);
+    console.log(units);
+  }, []);
+
   const dropDownClick = () => {
     addToggle("dropdown", "show-dropdown");
   };
 
-  const linkClick = () => {
+  const linkClick = (title) => {
     delClass("dropdown", "show-dropdown");
+    dispatch(setTitle(title));
   };
 
   return (
     <div className="filterBox">
       <div id="dropdown" className="dropdown">
         <div className="dropdown-select" onClick={dropDownClick}>
-          <span className="select">All Units</span>
+          <span className="select">{title}</span>
           <FaCaretDown />
         </div>
         <div className="dropdown-list">
-          <div className="dropdown-list__item" onClick={linkClick}>
-            All Units
-          </div>
-          <div className="dropdown-list__item" onClick={linkClick}>
-            Condition
-          </div>
-          <div className="dropdown-list__item" onClick={linkClick}>
-            Loop
-          </div>
-          <div className="dropdown-list__item" onClick={linkClick}>
-            Array
-          </div>
-          <div className="dropdown-list__item" onClick={linkClick}>
-            String
-          </div>
-          <div className="dropdown-list__item" onClick={linkClick}>
-            Pattern
-          </div>
-          <div className="dropdown-list__item" onClick={linkClick}>
-            God Mode
-          </div>
+          {units.map((unit) => (
+            <div
+              key={unit.name}
+              className="dropdown-list__item"
+              onClick={() => linkClick(unit.name)}
+            >
+              {unit.name}
+            </div>
+          ))}
         </div>
       </div>
     </div>
