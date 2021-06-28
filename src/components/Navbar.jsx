5f0392a6ab30708fb72ab30fc3ac46b23977cbd4
Zantaclaus <tarzuto@gmail.com>
2021-06-28 20:15:02 +0700

    Imprement css variable.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,7 +83,11 @@ function Navbar(props) {
           </ul>
         </div>
         <div className="nav__toggle" id="nav-toggle">
-          <GiHamburgerMenu size="3rem" color="black" onClick={menuClick} />
+          {isMoon ? (
+            <GiHamburgerMenu size="3rem" color="black" onClick={menuClick} />
+          ) : (
+            <GiHamburgerMenu size="3rem" color="white" onClick={menuClick} />
+          )}
         </div>
         <div className="theme__toggle" id="theme-toggle" onClick={themeClick}>
           {isMoon ? (
