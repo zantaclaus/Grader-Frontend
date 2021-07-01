d232bb871960097e7b97af8875619ed8f5904eb3
Zantaclaus <tarzuto@gmail.com>
2021-07-01 20:07:49 +0700

    Add getText from password manage.

--- src/pages/ManagePassword.jsx
+++ src/pages/ManagePassword.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/managePassword.css";
 
 function ManagePassword(props) {
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleInput = (text, setText) => {
+    setText(text);
+  };
+
+  const handlePasswordSubmit = () => {
+    console.log("password:", password);
+    console.log("newPassword:", newPassword);
+    console.log("confirmPassword:", confirmPassword);
+    // window.location = "/profile";
+  };
+
   return (
     <div className="password__container">
       <div className="password__box">
@@ -10,21 +26,40 @@ function ManagePassword(props) {
 
         <div className="password__inputBx">
           <span className="password__label">Password</span>
-          <input className="password__input" type="password" />
+          <input
+            className="password__input"
+            type="password"
+            value={password}
+            onChange={(e) => handleInput(e.currentTarget.value, setPassword)}
+          />
         </div>
 
         <div className="password__inputBx">
           <span className="password__label">New Password</span>
-          <input className="password__input" type="password" />
+          <input
+            className="password__input"
+            type="password"
+            value={newPassword}
+            onChange={(e) => handleInput(e.currentTarget.value, setNewPassword)}
+          />
         </div>
 
         <div className="password__inputBx">
           <span className="password__label">Confirm Password</span>
-          <input className="password__input" type="password" />
+          <input
+            className="password__input"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) =>
+              handleInput(e.currentTarget.value, setConfirmPassword)
+            }
+          />
         </div>
 
         <Link to="/profile">
-          <div className="password__btn">Submit</div>
+          <div className="password__btn" onClick={handlePasswordSubmit}>
+            Submit
+          </div>
         </Link>
       </div>
     </div>
