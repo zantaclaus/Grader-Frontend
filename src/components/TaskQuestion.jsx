1501b32d95c83ddb5630883c6757860bccc5e3b9
Zantaclaus <tarzuto@gmail.com>
2021-07-04 12:33:34 +0700

    Chnage logic display task question.

--- src/components/TaskQuestion.jsx
+++ src/components/TaskQuestion.jsx
@@ -5,61 +5,73 @@ import "../css/taskQuestion.css";
 
 function TaskQuestion(props) {
   const task = useSelector((state) => state.task);
-  const arrQuestion = task.question.replaceAll(" ", "&nbsp;").split("\n");
-  const arrInput1 = task.str_input_1.replaceAll(" ", "&nbsp;").split("\n");
-  const arrInput2 = task.str_input_2.replaceAll(" ", "&nbsp;").split("\n");
-  const arrInput3 = task.str_input_3.replaceAll(" ", "&nbsp;").split("\n");
-  const arrOutput1 = task.str_output_1.replaceAll(" ", "&nbsp;").split("\n");
-  const arrOutput2 = task.str_output_2.replaceAll(" ", "&nbsp;").split("\n");
-  const arrOutput3 = task.str_output_3.replaceAll(" ", "&nbsp;").split("\n");
-  const arrQ_input = task.q_input.replaceAll(" ", "&nbsp;").split("\n");
-  const arrQ_output = task.q_output.replaceAll(" ", "&nbsp;").split("\n");
-  const arrDetail = task.detail.replaceAll(" ", "&nbsp;").split("\n");
+  const jsxQuestion = task.question
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxInput1 = task.str_input_1
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxInput2 = task.str_input_2
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxInput3 = task.str_input_3
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxOutput1 = task.str_output_1
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxOutput2 = task.str_output_2
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxOutput3 = task.str_output_3
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxQ_input = task.q_input
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxQ_output = task.q_output
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
+  const jsxDetail = task.detail
+    .replaceAll(" ", "&nbsp;")
+    .replaceAll("\n", "<br/>");
 
   return (
     <div className="pdf__container">
       <div className="title">{task.title}</div>
       <div className="chaya">{task.chaya}</div>
       <div className="question">
-        {arrQuestion.map((line) => (
-          <div dangerouslySetInnerHTML={{ __html: line }} />
-        ))}
+        <div dangerouslySetInnerHTML={{ __html: jsxQuestion }} />
       </div>
       <div className="pattern">
         <div className="task__label">รูปแบบข้อมูลนำเข้า</div>
         <div className="pattern__detail">
-          {arrQ_input.map((line) => (
-            <div dangerouslySetInnerHTML={{ __html: line }} />
-          ))}
+          <div dangerouslySetInnerHTML={{ __html: jsxQ_input }} />
         </div>
       </div>
       <div className="pattern">
         <div className="task__label">รูปแบบข้อมูลส่งออก</div>
         <div className="pattern__detail">
-          {arrQ_output.map((line) => (
-            <div dangerouslySetInnerHTML={{ __html: line }} />
-          ))}
+          <div dangerouslySetInnerHTML={{ __html: jsxQ_output }} />
         </div>
       </div>
       <div className="tastcase">
         {(task.str_input_1 || task.str_output_1) && (
-          <TaskTable number="1" input={arrInput1} output={arrOutput1} />
+          <TaskTable number="1" input={jsxInput1} output={jsxOutput1} />
         )}
 
         {(task.str_input_2 || task.str_output_2) && (
-          <TaskTable number="2" input={arrInput2} output={arrOutput2} />
+          <TaskTable number="2" input={jsxInput2} output={jsxOutput2} />
         )}
 
         {(task.str_input_3 || task.str_output_3) && (
-          <TaskTable number="3" input={arrInput3} output={arrOutput3} />
+          <TaskTable number="3" input={jsxInput3} output={jsxOutput3} />
         )}
       </div>
       <div className="pattern">
         <div className="task__label">เพิ่มเติม</div>
         <div className="pattern__detail">
-          {arrDetail.map((line) => (
-            <div dangerouslySetInnerHTML={{ __html: line }} />
-          ))}
+          <div dangerouslySetInnerHTML={{ __html: jsxDetail }} />
         </div>
       </div>
     </div>
--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -12,14 +12,10 @@ function TaskTable({ number, input, output }) {
         </div>
         <div className="tastcase__table">
           <div className="table table__detail detail__input">
-            {input.map((line) => (
-              <div dangerouslySetInnerHTML={{ __html: line }} />
-            ))}
+            <div dangerouslySetInnerHTML={{ __html: input }} />
           </div>
           <div className="table table__detail detail__output">
-            {output.map((line) => (
-              <div dangerouslySetInnerHTML={{ __html: line }} />
-            ))}
+            <div dangerouslySetInnerHTML={{ __html: output }} />
           </div>
         </div>
       </div>
