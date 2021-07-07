e635ede1d2bc1e0e6babb46df19d213c6e46c4f3
Zantaclaus <tarzuto@gmail.com>
2021-07-07 20:05:46 +0700

    Add padding to Last Banned.

--- src/components/Banned.jsx
+++ src/components/Banned.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-import "../css/bannedFunction.css";
+import "../css/banned.css";
 
 function Banned({ title, tagline, lists }) {
   return (
     <div className="banned">
       <div className="guideBook__title">{title}</div>
       <div className="guideBook__tagline">{tagline}</div>
-      <div className="banned__content">
+      <div
+        className={
+          title === "Banned Function"
+            ? "banned__content banned__last"
+            : "banned__content"
+        }
+      >
         {lists.map((list) => (
           <span key={list.id} className="banned__list">
             {list.name}
