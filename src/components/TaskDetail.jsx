178f28491efbe319032ddc70b7824c8203e5d60b
Zantaclaus <tarzuto@gmail.com>
2021-07-12 00:45:12 +0700

    Built Tooltip.

--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../css/taskDetail.css";
 import { IoRefreshOutline } from "react-icons/io5";
+import Tooltip from "./Tooltip";
 
 function TaskDetail({ title, detail, header, content, setforceRender }) {
   return (
     <div className="task__detail">
       <div className={header}>{title}</div>
+      {title !== "Finished" && <Tooltip detail={detail} />}
       <div className={content}>{detail}</div>
       {title !== "Finished" && (
         <IoRefreshOutline
--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "../css/tooltip.css";
 
-function Tooltip(props) {
-  const [tooltip, setTooltip] = useState("");
+function Tooltip({ detail }) {
+  const [tooltip, setTooltip] = useState([]);
 
-  return <div className="tooltip__container"></div>;
+  useEffect(() => {
+    let result = [];
+    if (detail.includes("B")) result.push("B: Built Failed");
+    if (detail.includes("C")) result.push("C: Created Failed");
+    if (detail.includes("F")) result.push("F: Function is Banned");
+    if (detail.includes("L")) result.push("L: Library is Banned");
+    if (detail.includes("O")) result.push("O: Out of Buffer");
+    if (detail.includes("P")) result.push("P: Pass");
+    if (detail.includes("T")) result.push("T: Time out");
+    if (detail.includes("W")) result.push("W: Testcase Error");
+    if (detail.includes("X")) result.push("X: Runtime Error");
+    if (detail.includes("Y")) result.push("Y: Grader Error");
+    if (detail.includes("-")) result.push("-: Not Pass");
+    setTooltip(result);
+  }, [detail, tooltip]);
+
+  return (
+    <div className="tooltip__container">
+      {tooltip.map((text) => (
+        <div>{text}</div>
+      ))}
+    </div>
+  );
 }
 
 export default Tooltip;
