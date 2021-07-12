e64382d0f7b4f6488a302c6964d3826f757e367f
Zantaclaus <tarzuto@gmail.com>
2021-07-13 00:32:46 +0700

    Imprement LeaderBoard

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,70 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Aos from "aos";
-import team1 from "../images/team1.webp";
-import team2 from "../images/team2.webp";
-import team3 from "../images/team3.webp";
+import ProfileGroup from "./ProfileGroup";
+import Loading from "./Loading";
 import "../css/leaderBoard.css";
-// import axios from "axios";
+import axios from "axios";
 
 function LeaderBoard(props) {
-  // useEffect(() => {
-  //   const fetch = async () => {
-  //     const leaderBoard = await axios.get(
-  //       `https://api.ceboostup.com/api/leader`
-  //     );
-  //     console.log(leaderBoard.data);
-  //   };
-  //   fetch();
-  // });
+  const [leader, setLeader] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const reasult = await axios.get(`https://api.ceboostup.com/api/leader`);
+      const scoreBoard = reasult.data.scoreBoard;
+      const topLeader = [scoreBoard[0], scoreBoard[1], scoreBoard[2]];
+      setLeader(topLeader);
+    };
+    fetch();
+  }, []);
+  console.log(leader);
 
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
 
   return (
-    <div className="leader__block">
-      <div data-aos="zoom-in-up" className="container">
-        <header className="leader__header">
-          <h1 className="leader__title">Leader Board</h1>
-          <p>Top 3 ranking doesn't sort by score.</p>
-        </header>
-        <div className="leader__content">
-          <div className="leader__card">
-            <div className="face front">
-              <h1>T</h1>
-            </div>
-            <div className="face back">
-              <div className="back__content">
-                <img src={team1} alt="" />
-                <h1>เทพจ๊ากซ่า1</h1>
+    <React.Fragment>
+      {Object.keys(leader).length === 0 ? (
+        <Loading />
+      ) : (
+        <div className="leader__block">
+          <div data-aos="zoom-in-up" className="container">
+            <header className="leader__header">
+              <h1 className="leader__title">Leader Board</h1>
+              <p>Top 3 ranking doesn't sort by score.</p>
+            </header>
+            <div className="leader__content">
+              <div className="leader__card">
+                <div className="face front">
+                  <h1>T</h1>
+                </div>
+                <div className="face back">
+                  <div className="back__content">
+                    <ProfileGroup userDetail={leader[0]} />
+                    <h1>{leader[0].nickName}</h1>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="leader__card">
-            <div className="face front">
-              <h1>O</h1>
-            </div>
-            <div className="face back">
-              <div className="back__content">
-                <img src={team2} alt="" />
-                <h1>เทพจ๊ากซ่า2</h1>
+              <div className="leader__card">
+                <div className="face front">
+                  <h1>O</h1>
+                </div>
+                <div className="face back">
+                  <div className="back__content">
+                    <ProfileGroup userDetail={leader[1]} />
+                    <h1>{leader[1].nickName}</h1>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="leader__card">
-            <div className="face front">
-              <h1>P</h1>
-            </div>
-            <div className="face back">
-              <div className="back__content">
-                <img src={team3} alt="" />
-                <h1>เทพจ๊ากซ่า3</h1>
+              <div className="leader__card">
+                <div className="face front">
+                  <h1>P</h1>
+                </div>
+                <div className="face back">
+                  <div className="back__content">
+                    <ProfileGroup userDetail={leader[2]} />
+                    <h1>{leader[2].nickName}</h1>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </React.Fragment>
   );
 }
 
