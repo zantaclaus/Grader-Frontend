fd4641c243d3bf731db1574d034cef72207137d7
Zantaclaus <tarzuto@gmail.com>
2021-07-14 16:01:31 +0700

    Create Contact.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact(props) {
+  return (
+    <div>
+      <div className="contact__card">
+        <div className="contact__imgBx"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
