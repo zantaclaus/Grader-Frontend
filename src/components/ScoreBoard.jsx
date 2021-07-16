68e661e397367f5ac6dd01346fbe999c31416254
Zantaclaus <tarzuto@gmail.com>
2021-07-16 15:14:00 +0700

    Imprement Scoreboard layout.

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -4,9 +4,24 @@ import { useState } from "react";
 import "../css/scoreBoard.css";
 import { getGroup } from "../services/groupService";
 import Loading from "../components/Loading";
+import { paginate } from "../services/paginateService";
+import { IoMdArrowDropleftCircle } from "react-icons/io";
+import { IoMdArrowDroprightCircle } from "react-icons/io";
+import { useSelector } from "react-redux";
 
 function ScoreBoard() {
+  const userDetail = useSelector((state) => state.user.user);
   const [scoreBoard, setScoreBoard] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+
+  const pageIncreaseHandle = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const pageDecreaseHandle = () => {
+    setCurrentPage(currentPage - 1);
+  };
 
   useEffect(() => {
     const fetch = async () => {
@@ -25,6 +40,9 @@ function ScoreBoard() {
     fetch();
   }, []);
 
+  const sliceProblems = paginate(scoreBoard, currentPage, pageSize);
+  const lastPage = Math.ceil(scoreBoard.length / pageSize);
+
   return (
     <React.Fragment>
       {Object.keys(scoreBoard).length === 0 ? (
@@ -37,7 +55,7 @@ function ScoreBoard() {
               <p>Top 3 ranking doesn't sort by score.</p>
             </header>
             <div className="leader__content scoreboard__content">
-              <table>
+              <table className="noselect">
                 <thead>
                   <tr>
                     <th>#</th>
@@ -47,8 +65,15 @@ function ScoreBoard() {
                   </tr>
                 </thead>
                 <tbody>
-                  {scoreBoard.map((user) => (
-                    <tr key={user.id}>
+                  {sliceProblems.map((user) => (
+                    <tr
+                      key={user.id}
+                      className={
+                        userDetail.nickName === user.nickName
+                          ? "scoreboard__highlight"
+                          : ""
+                      }
+                    >
                       <td>{user.id}</td>
                       <td>{user.nickName}</td>
                       <td>{getGroup(user.group)[0].name}</td>
@@ -56,6 +81,28 @@ function ScoreBoard() {
                     </tr>
                   ))}
                 </tbody>
+                <tfoot>
+                  <tr>
+                    <td colSpan={4}>
+                      <div className="scoreboard__arrow__container">
+                        {currentPage != 1 && (
+                          <IoMdArrowDropleftCircle
+                            size="3.4rem"
+                            className="scoreboard__arrow"
+                            onClick={() => pageDecreaseHandle()}
+                          />
+                        )}
+                        {currentPage != lastPage && (
+                          <IoMdArrowDroprightCircle
+                            size="3.4rem"
+                            className="scoreboard__arrow"
+                            onClick={() => pageIncreaseHandle()}
+                          />
+                        )}
+                      </div>
+                    </td>
+                  </tr>
+                </tfoot>
               </table>
             </div>
           </div>
