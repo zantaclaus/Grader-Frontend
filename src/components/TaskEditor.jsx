9f0eedd67fb154f5ade17da5a5a9161b9245c0c2
Zantaclaus <tarzuto@gmail.com>
2021-07-18 18:49:47 +0700

    add highlight C.

--- src/components/TaskEditor.jsx
+++ src/components/TaskEditor.jsx
@@ -10,6 +10,7 @@ import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
 
 function TaskEditor({ code, setCode, status }) {
   const userDetail = useSelector((state) => state.userDetail.user);
@@ -30,7 +31,7 @@ function TaskEditor({ code, setCode, status }) {
       <Editor
         value={code}
         onValueChange={(code) => setCode(code)}
-        highlight={(code) => highlight(code, languages.c, "c")}
+        highlight={(code) => highlight(code, languages.cpp, "cpp")}
         padding={10}
         className="editor"
       />
