29fb65d33cbc20e4b7b32d562aa2265e080949e1
Zantaclaus <tarzuto@gmail.com>
2021-07-18 18:48:45 +0700

    Change highlight to C.

--- src/components/TaskEditor.jsx
+++ src/components/TaskEditor.jsx
@@ -9,7 +9,7 @@ import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/components/prism-clike";
-import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-c";
 
 function TaskEditor({ code, setCode, status }) {
   const userDetail = useSelector((state) => state.userDetail.user);
@@ -30,7 +30,7 @@ function TaskEditor({ code, setCode, status }) {
       <Editor
         value={code}
         onValueChange={(code) => setCode(code)}
-        highlight={(code) => highlight(code, languages.cpp, "cpp")}
+        highlight={(code) => highlight(code, languages.c, "c")}
         padding={10}
         className="editor"
       />
