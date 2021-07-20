8b9b7f446e95516611ec7db210435bc1f1c136bf
Zantaclaus <tarzuto@gmail.com>
2021-07-20 15:53:49 +0700

    show userRank.

--- src/components/ProfileRank.jsx
+++ src/components/ProfileRank.jsx
@@ -19,7 +19,8 @@ function ProfileRank(props) {
         <div className="card__content rank__content">
           <div className="rank__team">
             <h3>You are</h3>
-            <BsQuestionCircleFill className="rank__rank" />
+            <h1>{userDetail.userRank}</h1>
+            {/* <BsQuestionCircleFill className="rank__rank" /> */}
             {/* <img className="team__logo" src={team2} alt="" /> */}
           </div>
           <div className="rank__score">
